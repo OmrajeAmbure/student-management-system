f18539479dc8c0d267325a578ffe8fc0efeb4658
OmrajeAmbure <omrajeambure27@gmail.com>
2025-09-14 19:41:45 +0530

    Update some file

--- student-management-frontend/src/api/axios.js
+++ student-management-frontend/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "http://localhost:8080", // no need for http://localhost:8080
 });
 
-// Add token automatically
+// Attach JWT token automatically
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
     if (token) {

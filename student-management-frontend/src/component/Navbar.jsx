e037a2366b3670bc9f3427a0574a8d079258239a
OmrajeAmbure <omrajeambure27@gmail.com>
2025-09-15 18:03:47 +0530

    Add Some New File

--- student-management-frontend/src/component/Navbar.jsx
+++ student-management-frontend/src/component/Navbar.jsx
@@ -13,7 +13,24 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
                 <Link className="navbar-brand" to="/">StudentApp</Link>
-                <div className="collapse navbar-collapse justify-content-end">
+
+                {/* 👇 This is the missing toggler */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div
+                    className="collapse navbar-collapse justify-content-end"
+                    id="navbarSupportedContent"
+                >
                     <ul className="navbar-nav align-items-center">
                         {!token ? (
                             <>

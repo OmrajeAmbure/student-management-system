3c8193ba1c6565432108714a3ef9c99fad32b0e9
OmrajeAmbure <omrajeambure27@gmail.com>
2025-09-15 17:11:11 +0530

    Add Some New File

--- student-management-frontend/src/api/axios.js
+++ student-management-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080", // no need for http://localhost:8080
+    baseURL: "https://student-management-system-jitf.onrender.com", // no need for http://localhost:8080
 });
 
 // Attach JWT token automatically
